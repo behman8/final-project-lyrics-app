18cdd11e4db82bd49b190b411ad2ce989bea6b07
Branden <brandenehman45@gmail.com>
2022-01-29 18:43:43 -0700

    Finished login component, and app component.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import NavBar from './Containers/NavBar';
 import SongsContainer from './Containers/SongsContainer';
+import SongShow from './Components/SongShow';
+import Login from './Components/Login';
+import Home from './Components/Home';
 
 function App() {
 
@@ -25,14 +28,23 @@ function App() {
         <Routes>
           <Route exact path="/songs" element={<SongsContainer user={user} />}></Route>
           <Route exact path="/songs/:id" element={<SongShow />}></Route>
-          <Route exact path="/songs" element={<SongsContainer user={user} />}></Route>
-          <Route exact path="/songs" element={<SongsContainer user={user} />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
         </Routes>
       </main>
     </div>
   );
   } else {
-
+    <div>
+      <NavBar/>
+      {<Login onLogin={setUser} />}
+      <main>
+        <h2>Please login to view more!</h2>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
+    </div>
   }
 }
 
